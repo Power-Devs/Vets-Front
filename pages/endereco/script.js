40266b0ccfd344517773aa2bb358ea3c87c0c308
Marcos Martins <marcos_36ma@hotmail.com>
2021-04-17 22:28:32 -0300

    criacao do script para retornar endereco

--- pages/endereco/script.js
+++ pages/endereco/script.js
@@ -1,3 +1,32 @@
-function foo(){
-    console.log(document.querySelector("#busca"));
+async function searchAddress() {
+    let userAddress = document.querySelector("#busca").value;
+
+    let userLocation = await googleGeolocationService(userAddress);
+
+    console.log(userLocation);
+    //TODO: Inserir aqui a chamada a tela do mapa informando userLocation
+}
+
+async function googleGeolocationService(endereco){
+
+    let apiKey = "AIzaSyCK0_iwIReeTUn6NdEiS26vedXlb_Yk1xA";
+    let userEndereco = encodeURI(endereco);
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${userEndereco}&key=${apiKey}`;
+    let location;
+    let result = await requestHandler(url);
+
+
+    if(result.status === "OK"){
+        location = result.results[0].geometry.location;
+        return location;
+    }else{
+        //TODO: Inserir aqui um aviso erro na interface.
+        window.alert("request invalida")
+    }
+}
+
+async function requestHandler( URL_Request ){
+    let response = await fetch( URL_Request );
+    let data = await response.json();
+    return data;
 }
