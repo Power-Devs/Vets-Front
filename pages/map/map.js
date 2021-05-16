e2d2eb4310e0814e4125901f013e3478ea6ae22c
Marcos Martins <marcos_36ma@hotmail.com>
2021-05-16 00:21:59 -0300

    ajustado script js e adicionado tags de loc

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,179 +1,41 @@
-let map, infoWindow;
+let map;
 
-var latitude = -23.6508475
-var longitude = -46.7567901
-// joao, arrumar isso. acha um jeito de conseguir pegar as informações do json e criar variavel igual nas linhas 3 e 4
+var loc = {
+  "latitude":-23.659962,
+  "longitude": -46.769241,
+}
 
 
+//Inicia o mapa com a lat e long informados
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: latitude, lng: longitude },
+    center: { 
+      lat: loc.latitude, 
+      lng: loc.longitude 
+    },
     zoom: 20,
-    styles: [
-      {
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#bdbdbd"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#ffffff"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dadada"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#c9c9c9"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      }
-    ]
+    //styles: styles carregar depois o json styles
   });
-  const geocoder = new google.maps.Geocoder();
-  const infowindow = new google.maps.InfoWindow();
-  document.getElementById("submit").addEventListener("click", () => {
-    geocodeLatLng(geocoder, map, infowindow);
+  let mark1 = new google.maps.Marker({
+    position: { lat: loc.latitude, lng: loc.longitude },
+    map: map
+  });
+
+  const svgMarker = {
+    path:
+      "M40 55C51.0457 55 60 46.0457 60 35C60 23.9543 51.0457 15 40 15C28.9543 15 20 23.9543 20 35C20 46.0457 28.9543 55 40 55Z",
+    fillColor: "#004D61",
+    fillOpacity: 1,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,
+    anchor: new google.maps.Point(15, 30),
+  };
+
+  let mark2= new google.maps.Marker({
+    position: { lat: -23.659702, lng: -46.769021 },
+    map: map,
+    icon: svgMarker
   });
 }
 
@@ -183,32 +45,3 @@ function get_text_location(){
 
   localizacao.value = novoValor;
 }
-
-   // tem q deixar setado pra iniciar como variavel, pra pegar a lti e long da outra tela, q o marcos fez
-      
-function get_text_location(){
-  const novoValor = "add conversao de lat e long para endereco manuscrito";
-  const localizacao = document.getElementById("localizacao");
-
-  localizacao.value = novoValor;
-}
-
-var marker = new google.maps.Marker({
-  position: { lat: -23.6508475, lng: -46.7567901 },
-  label:"A",
-  title:"Location Name"
-});
-marker.setMap(map);
-
-
-function myFunction() {
-  var x =
-  document.getElementById("busca").innerHTML = text;
-  
-  document.getElementById(
-  "demo").innerHTML = x;
-  }
-
-  // input ja escrito
-  var test = "test";
-    document.getElementById("localizacao").value = test;
