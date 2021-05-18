0062d5154f96db7bfd5dc0d2acc093f6ef272de0
Marcos Martins <marcos_36ma@hotmail.com>
2021-05-17 23:09:47 -0300

    localizacao de endereco e marcacao

---
+++ pages/map/getVets.js
@@ -0,0 +1,21 @@
+const defineLoc = (endereco) => {
+    console.log(local)
+
+    vets = [
+        { 
+          "lat": -23.659702,
+          "lng": -46.769021 
+        },
+        { 
+          "lat": -24.659702,
+          "lng": -47.769021 
+        }
+    ]
+
+    map.setCenter(endereco);
+}
+
+
+
+
+
--- pages/map/map.js
+++ pages/map/map.js
@@ -1,47 +1,64 @@
-let map;
-
-var loc = {
+let local =  {
   "latitude":-23.659962,
   "longitude": -46.769241,
 }
 
+let map;
+let vets = [];
 
 //Inicia o mapa com a lat e long informados
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { 
-      lat: loc.latitude, 
-      lng: loc.longitude 
+      lat: local.latitude , 
+      lng: local.longitude 
     },
     zoom: 20,
-    //styles: styles carregar depois o json styles
-  });
-  let mark1 = new google.maps.Marker({
-    position: { lat: loc.latitude, lng: loc.longitude },
-    map: map
+    disableDefaultUI: true, //remove ui do map
   });
 
   const svgMarker = {
-    path:
-      "M40 55C51.0457 55 60 46.0457 60 35C60 23.9543 51.0457 15 40 15C28.9543 15 20 23.9543 20 35C20 46.0457 28.9543 55 40 55Z",
-    fillColor: "#004D61",
-    fillOpacity: 1,
-    strokeWeight: 0,
-    rotation: 0,
-    scale: 2,
-    anchor: new google.maps.Point(15, 30),
+  path:
+    "M40 55C51.0457 55 60 46.0457 60 35C60 23.9543 51.0457 15 40 15C28.9543 15 20 23.9543 20 35C20 46.0457 28.9543 55 40 55Z",
+  fillColor: "#004D61",
+  fillOpacity: 1,
+  strokeWeight: 0,
+  rotation: 0,
+  scale: 1.5,
+  anchor: new google.maps.Point(15, 30), // Verificar a precisão do marcador
   };
 
-  let mark2= new google.maps.Marker({
-    position: { lat: -23.659702, lng: -46.769021 },
+  const myLocationMarker = {
+  path:
+  "M40 55C51.0457 55 60 46.0457 60 35C60 23.9543 51.0457 15 40 15C28.9543 15 20 23.9543 20 35C20 46.0457 28.9543 55 40 55Z",
+  fillColor: "red",
+  fillOpacity: 1,
+  strokeWeight: 0,
+  rotation: 0,
+  scale: 1.5,
+  };
+
+  //marca o loca do usuario, que é o mesmo do center
+  let mark1 = new google.maps.Marker({
+    position: { 
+      lat:local.latitude, 
+      lng: local.longitude
+    },
     map: map,
-    icon: svgMarker
+    icon: myLocationMarker
   });
-}
 
-function get_text_location(){
-  const novoValor = "add conversao de lat e long para endereco manuscrito";
-  const localizacao = document.getElementById("localizacao");
+  //Exemplo icone marker  de um vet
+ 
+  for (const vet of vets ) {
+    new google.maps.Marker({
+      position: { 
+        lat: vet.lat,
+        lng: vet.lng
+      },
+      map: map,
+      icon: svgMarker
+    });
+  }
 
-  localizacao.value = novoValor;
 }
